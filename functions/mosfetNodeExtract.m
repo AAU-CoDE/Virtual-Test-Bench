56e391e5fda3477add9a549ad6d7fa3fb2485e3a
mikezaech <98896982+mikezaech@users.noreply.github.com>
2023-03-27 10:50:48 +0200

    Fixed skipped header lines

--- functions/mosfetNodeExtract.m
+++ functions/mosfetNodeExtract.m
@@ -1,13 +1,19 @@
-function mosfetNodeList = mosfetNodeExtract(LTlibPath,mosfetModel)
+function mosfetNodeList = mosfetNodeExtract(LTlibPath,mosfetFileName,mosfetModel)
 % MOSFETNODEEXTRACT returns the line in the netlist which includes the connections/nodes of the SPICE model (.subckt model d s g etc)
 % This is used to properly define the connections in the netlist
 %
 % INPUT: 
 % LTlibPath: Path to directory where .lib file is stored (string)
-% mosfetModel: Name of the Mosfet model/.lib file (string)
-    libraryFile = readtable(append(LTlibPath,mosfetModel,'.lib'),'FileType','text');
+% mosfetFileName:  Name of the Mosfet .lib file as stored in the directory
+% (string)
+% mosfetModel: Name of the Mosfet model inside the .lib model. Typically
+% stated after the first .subckt statement. (string)
+
+ %%
+    libraryFile = readtable(append(LTlibPath,mosfetFileName,'.lib'),'FileType','text','NumHeaderLines',0);
     libraryFile = table2cell(libraryFile);
     modelLibText = libraryFile(:,1);
+
     for n = 1:numel(modelLibText)
       if contains(modelLibText(n),append(".subckt ",mosfetModel)) == 1
           mosfetNodeListIdx = n;
