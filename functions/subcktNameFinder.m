c5196f3377b3ec2bb6c69ce1b4442ebc3fa928bd
mikezaech <98896982+mikezaech@users.noreply.github.com>
2023-03-31 10:38:35 +0200

    cell -> char

--- functions/subcktNameFinder.m
+++ functions/subcktNameFinder.m
@@ -18,16 +18,16 @@ function mosfetModel = subcktNameFinder(userDef)
     % 
     subcktCnt = 1;
     for n = 1:numel(modelLibText)
-      if contains(modelLibText(n),".subckt ") == 1 || contains(modelLibText(n),".SUBCKT ")
+      if contains(modelLibText(n),'.subckt ') == 1 || contains(modelLibText(n),'.SUBCKT ')
           mosfetNodeListIdx = n;
           mosfetNodeList = modelLibText(mosfetNodeListIdx);
             % remove .subckt 
-            mosfetNodeList = replace(mosfetNodeList,".subckt ","");
-            mosfetNodeList = replace(mosfetNodeList,".SUBCKT ","");
+            mosfetNodeList = replace(mosfetNodeList,'.subckt ','');
+            mosfetNodeList = replace(mosfetNodeList,'.SUBCKT ','');
             % convert to string
             mosfetNodeList = char(mosfetNodeList);
             % Remove everything after next space
-            space = strfind(mosfetNodeList," ");
+            space = strfind(mosfetNodeList,' ');
             mosfetFileList{subcktCnt,:} = mosfetNodeList(1:space(1)-1);
             subcktCnt = subcktCnt + 1;
       end
@@ -35,10 +35,10 @@ function mosfetModel = subcktNameFinder(userDef)
 
     % Confirm
     if exist('mosfetNodeList','var') == 1  
-        disp(" ")
-        disp("Consider defining the desired model in the beginning of the script.")
-        disp(" ")
-        modelConfirmed = input(append('Please confirm: The device name to be extracted is: ',mosfetFileList{1,:}, ' [Y/N] (Press ENTER to confirm)'),"s" );
+        disp(' ')
+        disp('Consider defining the desired model in the beginning of the script.')
+        disp(' ')
+        modelConfirmed = input(append('Please confirm: The device name to be extracted is: ',mosfetFileList{1,:}, ' [Y/N] (Press ENTER to confirm)'),'s' );
 
         if isempty(modelConfirmed)
             modelConfirmed = 'Y';
@@ -50,15 +50,15 @@ function mosfetModel = subcktNameFinder(userDef)
                 [deviceIdx, tf] = listdlg('PromptString',{'Select the device name from the list below:',''},'SelectionMode','single','ListString',mosfetFileList);
                 mosfetModel = mosfetFileList(deviceIdx);
             otherwise 
-               disp("Invalid Answer.");
+               disp('Invalid Answer.');
         end   
         
     else 
-        disp(" ")
+        disp(' ')
         disp('Could not parse .lib file (might be encrypted). Confirm or enter the name of the device model')
-        disp(" ")
-        disp("Consider defining the desired model in the beginning of the script.")
-        modelNameIsSame = input('Is the filename of the .lib file the same as the device model (e.g. G2R80MT18J)? [Y/N] (Press ENTER to confirm)',"s" );
+        disp(' ')
+        disp('Consider defining the desired model in the beginning of the script.')
+        modelNameIsSame = input('Is the filename of the .lib file the same as the device model (e.g. G2R80MT18J)? [Y/N] (Press ENTER to confirm)','s' );
         if isempty(modelNameIsSame)
             modelNameIsSame = 'Y';
         end
@@ -66,10 +66,11 @@ function mosfetModel = subcktNameFinder(userDef)
             case {'Y','y',1}
                 mosfetModel = mosfetLibFileName;
             case {'N','n',0}
-                mosfetModel = input('Manually enter the name of the device model (e.g. G2R80MT18J). (Press ENTER to confirm)',"s" );
+                mosfetModel = input('Manually enter the name of the device model (e.g. G2R80MT18J). (Press ENTER to confirm)','s' );
             otherwise 
-               disp("Invalid Answer.");
+               disp('Invalid Answer.');
         end 
-    disp("Consider defining the desired model in the beginning of the script.")
+    disp('Consider defining the desired model in the beginning of the script.')
     end
+    mosfetModel = char(mosfetModel);
 end
