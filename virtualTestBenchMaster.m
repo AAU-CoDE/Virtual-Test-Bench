319b603ce2823c858d4c1f9353e297e06a3325b4
mikezaech <98896982+mikezaech@users.noreply.github.com>
2024-01-19 14:49:38 +0100

    added Ciss(Vds)

--- virtualTestBenchMaster.m
+++ virtualTestBenchMaster.m
@@ -6,12 +6,14 @@ warning off
 
 %% Specify MOSFET Model Filename 'mosfetFileName'.lib:
 
-userDef. mosfetLibFileName = ''; % Specify the name of the .lib file, as found in your directory. (without .lib)
+ userDef. mosfetLibFileName = 'CPM3-1200-0075A'; % Specify the name of the .lib file, as found in your directory. (without .lib)
 % E.g.: userDef.mosfetLibFileName = 'C2M0160120D - Packaged';
 
 % If .lib file name is different from model file name (after .subckt
 % statement within .lib file), or .lib file contains several models, but you know which model you want to extract, you can define the model name here and skip the dialogue 
 % E.g.: userDef.mosfetModel = 'C2M0160120D';
+
+% userDef.mosfetModel = 'CPM3-1200-0075A';
 
 %% Set up paths
 % Automatically find working directory (No need to touch this)
@@ -34,15 +36,26 @@ Tj_array = 0.1:25:175;
 plotOn = 1; % Want a plot? 1 = Yes, 0 = No
 rdson = rdsonTjTestBench(Id,Vgs,Tj_array,userDef,plotOn);
 
-%% Coss(Vds)/Qoss(Vds)
+% Coss(Vds)/Qoss(Vds)
 % Coss - Test Conditions
 fcoss = 1e6; % 1MHz -  AC frequency
 VdsMax = 1200; % Drain-Source Voltage Limit
 VdsMin = 1e-2; % Lower Voltage Limit
 nSampleTot = 25; % Nr of samples
 
+
 plotOn = 1; % Want a plot? 1 = Yes, 0 = No
 coss = cossVdsTestBench(fcoss,[VdsMin,VdsMax],nSampleTot,userDef,plotOn);
+
+%% Ciss(Vds)
+fciss = 1e6; % 1MHz -  AC frequency
+VdsMax = 650; % Drain-Source Voltage Limit
+VdsMin = 1e-2; % Lower Voltage Limit
+nSampleTot = 25; % Nr of samples
+
+plotOn = 1; % Want a plot? 1 = Yes, 0 = No
+ciss = cissVdsTestBench(fciss,[VdsMin,VdsMax],nSampleTot,userDef,plotOn);
+
 
 %%
 disp("EXTRACTION FINISHED")
